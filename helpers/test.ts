ad076804c838a07ec5590275efc162a65fd7f51f
artem.o <artem.o@appstockus.com>
2024-05-10 17:04:56 +0300

    init commit

--- helpers/test.ts
+++ helpers/test.ts
@@ -1,6 +1,6 @@
 import {generateImagesLinks} from "./imgen";
 
-const cookie = `18S9cX_1PtgZFX4G7JYUvVDHtcdTpYWWMKo-8Ax3aGLjgcE_MlMpcVf-_9a8EN2VP5c5gh975cJHi6tEFs-gycf5rY1fcnONuBMZMu2CvZbiB5Vs6Rcc97FQRxekQ_ocsuYqBsBG6cqwmnSqTFyKaipfw3S43tSDDPCoKL8Z-rn_ArtzAzdIDa-qSp0jXQubaCZ1J4Di4yqQ9ns8PQ_-qpA`;
+const cookie = ``;
 
 const prompt = `Wizards cooking in KFC kitchen, cctv footage`;
 
